a80eb6585f69abae86a549a07a0ad5370dffb6ea
gregwhitaker <gwhitake@gmail.com>
2016-05-30 19:46:07 -0700

    Adding licenses

--- src/main/java/com/github/gregwhitaker/fireforget/Server.java
+++ src/main/java/com/github/gregwhitaker/fireforget/Server.java
@@ -1,27 +1,55 @@
+/*
+ * Copyright 2016 Greg Whitaker
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.github.gregwhitaker.fireforget;
 
 import io.netty.bootstrap.ServerBootstrap;
-import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelInitializer;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.handler.logging.LoggingHandler;
 
 import java.net.InetSocketAddress;
 
+/**
+ *
+ */
 public class Server {
     private final InetSocketAddress bindAddress;
 
+    /**
+     * Initializes the server.
+     *
+     * @param bindAddress socket address where the server will be listening for reactivesocket connections
+     */
     public Server(final InetSocketAddress bindAddress) {
         this.bindAddress = bindAddress;
     }
 
+    /**
+     *
+     * @throws Exception
+     */
     public void start() throws Exception {
         ServerBootstrap server = new ServerBootstrap();
         server.group(new NioEventLoopGroup(1), new NioEventLoopGroup(4))
                 .channel(NioServerSocketChannel.class)
                 .handler(new LoggingHandler())
-                .childHandler(new ForgetHandler());
+                .childHandler(new ReactiveSocketChannelInitializer());
 
         server.bind(bindAddress).sync();
     }
@@ -29,10 +57,10 @@ public class Server {
     /**
      *
      */
-    class ForgetHandler extends SimpleChannelInboundHandler<Object> {
+    class ReactiveSocketChannelInitializer extends ChannelInitializer {
 
         @Override
-        protected void channelRead0(ChannelHandlerContext ctx, Object msg) throws Exception {
+        protected void initChannel(Channel ch) throws Exception {
 
         }
     }
